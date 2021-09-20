060694841a5d922a5d10fad971897b03eae4478b
Himanshu Raj <63577940+raj13himanshu@users.noreply.github.com>
2021-09-20 23:00:57 +0530

    Add files via upload

---
+++ src/Cards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Heading from "./Heading";
+
+function Card(props) {
+  // console.log(props);
+
+  return (
+    <>
+      <div className="cards">
+        <div className="card">
+          <img src={props.imgsrc} alt="myPic" className="card__img" />
+
+          <div className="card__info">
+            <span className="card__category">{props.title} </span>
+
+            <Heading sname={props.sname} />
+
+            <a href={props.link} target="blank">
+              <button> Watch Now </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Card;
---
+++ src/Sdata.jsx
@@ -0,0 +1,48 @@
+const Sdata = [
+  {
+    id: 1,
+    sname: "DARK",
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 2,
+    sname: "Stranger Things",
+    imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 3,
+    sname: "The Witcher",
+    imgscr: "https://wallpapercave.com/wp/wp5852734.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80189685",
+  },
+
+  {
+    id: 4,
+    sname: "the Vampire Diaries ",
+    imgscr: "https://images.justwatch.com/poster/8589251/s592",
+    title: "azon Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 5,
+    sname: "My First-2 Love",
+    imgscr:
+      "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81026700?source=35",
+  },
+  {
+    id: 6,
+    sname: "DARK",
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+];
+
+export default Sdata;
